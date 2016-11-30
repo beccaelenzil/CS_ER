06d8267ef4e98ea61ed24e316e3614d303689c61
ejrseattle <ezrarobinson@Ezras-MacBook-Pro-1193.local>
2016-11-30 10:51:25 -0800

    first commit

---
+++ ScoringTheGame/CamelGame.py
@@ -0,0 +1,7 @@
+print 'Welcome to HiroChase!'
+print 'You did not do your homework and have to make your way to the door.'
+print 'Your teacher wants to confront you and is chasing down! Survive the school day and outrun your teacher.'
+
+done = False
+
+while not done:
