a3523d8151851ae6e2868086c0e9c82bfb9d3e0f
ejrseattle <ezrarobinson@Ezras-MacBook-Pro-1193.local>
2016-11-30 16:53:53 -0800

    11-30 5pm

--- ScoringTheGame/CamelGame.py
+++ ScoringTheGame/CamelGame.py
@@ -1,7 +1,42 @@
+import random
+
 print 'Welcome to HiroChase!'
-print 'You did not do your homework and have to make your way to the door.'
-print 'Your teacher wants to confront you and is chasing down! Survive the school day and outrun your teacher.'
+print 'You did not do your homework and have to make your way to your car.'
+print 'Your teacher wants to confront you and is chasing down, but you\'re wearing new kicks! Survive the school day and outrun your teacher.'
 
 done = False
 
 while not done:
+    print "A. Drink a Red Bull."
+    print "B. Flee at a speed-walk."
+    print "C. Flee at a run."
+    print "D. Stop and massage your feet."
+    print "E. Status check"
+    print "Q. Quit."
+
+    metersTraveled = 0
+    thirst = 0
+    footTiredness = 0
+    drinksLeft = 3
+    teacherMeters = -20
+    dist = 0
+
+    userInput = raw_input("What will you do?: ")
+
+    if userInput.upper() == "Q":
+        done = True
+    elif userInput.upper() == "E":
+        print "Meters traveled:  " + metersTraveled
+        print "Red Bulls in Bag:  " + drinksLeft
+        print "Your teacher is " + (metersTraveled - teacherMeters) + " meters away."
+    elif userInput.upper() == "D":
+        footTiredness = 0
+        print "Your feet have been soothed."
+        teacherMeters += random.randint(7, 14)
+    elif userInput.upper() == "C":
+        dist = random.randint(10, 21)
+        metersTraveled += dist
+        print "You ran " + dist + " meters."
+        thirst += 1
+        footTiredness += random.randint(1, 4)
+        teacherMeters += random.randint(7, 14)
