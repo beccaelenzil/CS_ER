0af006940a60890cb0415365491234937c506a89
ejrseattle <ezrarobinson@Ezras-MacBook-Pro-1193.local>
2017-03-29 10:50:32 -0700

    suuuuuper lame scoresFor done

--- AdvancedCS/Trinket/Board.py
+++ AdvancedCS/Trinket/Board.py
@@ -4,6 +4,9 @@
 # Name: EZ-Rob
 #
 
+import random
+
+
 class Board:
     """ a datatype representing a C4 board
         with an arbitrary number of rows and cols
@@ -159,7 +162,7 @@ class Board:
         won = False
         self.clear()
 
-        print "Welcome to gonnegt fore!"
+        print "Welcome to Gonnegt Fore!"
 
         while not done:
 
@@ -185,3 +188,72 @@ class Board:
                 if self.isFull() and not won:
                     print "The board is full and nobody has won."
                     done = True
+
+
+class Player:
+
+    def __init__(self, ox, tbt, ply):
+        self.ox = ox
+        self.tbt = tbt
+        self.ply = ply
+
+    def __repr__(self):
+        s = "Player for " + self.ox + "\n"
+        s += "  with tiebreak type: " + self.tbt + "\n"
+        s += "  and ply == " + str(self.ply) + "\n\n"
+        return s
+
+    def oppCh(self):
+        if self.ox == 'X':
+            return 'O'
+        else:
+            return 'X'
+
+    def scoreBoard(self, b):
+        if b.winsFor(self.ox):
+            return 100.0
+        elif b.winsFor(self.oppCh()):
+            return 0.0
+        else:
+            return 50.0
+
+    def tiebreakMove(self, scores):
+
+        maxIndices = []
+        for i in range(len(scores)):
+            if scores[i] == max(scores):
+                maxIndices.append(i)
+
+        if len(maxIndices) > 1:
+            if self.tbt == 'LEFT':
+                return maxIndices[0]
+            elif self.tbt == 'RIGHT':
+                return maxIndices[len(maxIndices)-1]
+            else:
+                return maxIndices[random.randrange(len(maxIndices))]
+        else:
+            return maxIndices[0]
+
+    def scoresFor(self, b):
+        scores = []
+
+        for col in range(b.width):
+            b.addMove(col, self.ox)
+            if b.allowsMove(col):
+                if self.scoreBoard(b) == 100.0:
+                    scores.append(self.scoreBoard(b))
+                else:
+                    b.addMove(col, self.oppCh())
+                    scores.append(self.scoreBoard(b))
+                    b.delMove(col)
+            else:
+                scores.append(-1)
+            b.delMove(col)
+
+        for col in range(b.width):
+            b.addMove(col, self.oppCh())
+            if scores[col] == 50.0:
+                scores[col] = self.scoreBoard(b)
+            b.delMove(col)
+
+        return scores
--- AdvancedCS/Trinket/Player.py
+++ AdvancedCS/Trinket/Player.py
@@ -1,4 +1,4 @@
-import Board
+from Board import Board
 import random
 
 class Player:
