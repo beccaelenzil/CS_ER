2c3c5a00e8983001fe3a58a8628e2c4795f2e00e
ejrseattle <ezrarobinson@Ezras-MacBook-Pro-1193.local>
2017-04-14 11:46:10 -0700

    main quickSort algorithm works, but iteration doesn't

--- AdvancedCS/Trinket/Sorting.py
+++ AdvancedCS/Trinket/Sorting.py
@@ -1,18 +1,56 @@
+# File: Sorting.py
+# Versions: Python 2.7.13
+# Name: Ezra Robinson
+# Date: 4/20/17
+# Desc: various sorting methods
+# Usage:
+
+import random
+
+
+# creates a list of numbers between 0 and listRange and lenght listLength
+def makeList(listLength, listRange):
+    return random.sample(range(0, listRange+1), listLength)
+
+
+# sorts alist using quick sort algorithm
 def QuickSort(alist):
-    pivot = alist[0]
-    list = alist
+    currentList = alist
+    pivot = currentList[0]
 
-    for i in len(list):
-        if list[i] >= pivot:
-            big_num = list[i]
+    for i in range(len(currentList) - 1):
+        if currentList[i] >= pivot:
 
-            j = list[len(list)-1]
-            while list[j] >= pivot:
+            j = len(currentList) - 1
+            while currentList[j] >= pivot and j >= i:
                 j -= 1
 
-            list[i],list[j] = list[j],list[i]
+            if j > i:
+                currentList[i],currentList[j] = currentList[j],currentList[i]
 
-    print list
+        print currentList
+
+    alist = currentList
+
+    midpoint = 0
+    for item in currentList:
+        if item < pivot:
+            midpoint += 1
+
+    newList = alist[:midpoint]
+    if len(newList) > 1:
+        QuickSort(newList)
+        for i in newList:
+            alist[i] = newList[i]
+
+    newList = alist[(midpoint + 1):]
+    if len(newList) > 1:
+        QuickSort(newList)
+        for i in newList:
+                alist[i] = newList[i]
+
+    print alist
+
 
 def test():
     alist = [39, 30, 45, 33, 20, 61, 36, 5, 31, 64, 22, 10, 21, 25, 80, 86, 63, 27, 85, 2, 71, 4, 5]
